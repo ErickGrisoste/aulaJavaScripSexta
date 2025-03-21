350616d6748e395f8ef003eef6b802468501821a
ErickGrisoste <grisosteerick25@gmail.com>
2025-03-21 20:24:32 -0300

    lampada top

---
+++ script.js
@@ -0,0 +1,8 @@
+function ligar(){
+    let lam = document.getElementById('lampada');
+    lam.src = "img/img/lampada_acesa.png"
+}
+
+function desligar(){
+    document.getElementById('lampada').src = "img/img/lampada_apagada.png";
+}
